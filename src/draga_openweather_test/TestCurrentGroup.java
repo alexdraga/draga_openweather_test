33d3bb8d30c6e9abb482d67a0bf0b0d952f63f29
Aleksandr Draga <a_draga@wargaming.net>
2016-03-31 16:47:29 +0300

    Add tests with slash for TestCurrentGroup.

--- src/draga_openweather_test/TestCurrentGroup.java
+++ src/draga_openweather_test/TestCurrentGroup.java
@@ -300,5 +300,254 @@ public class TestCurrentGroup {
         // TODO: write JSON validation
     }
 
+    @Test
+    public void testCurrentGroupTooLongRequestSlash() throws IOException, ParseException{
+        Integer expectedCode = 414;
+        String app_id = new String(new char[9000]).replace("\0", "a");
+        String params = "/?appid=" + app_id;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+    }
+
+    @Test
+    public void testCurrentGroupUnknownKeySlash() throws IOException, ParseException{
+        Integer expectedCode = 401;
+        Integer expectedCod = 401;
+        String expectedMessage =
+                "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.";
+        String params = "/?yyyyyy=yyyy";
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertTrue("No message field is present",
+                ((JSONObject) responseBody).containsKey("message"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        assertEquals("Wrong message in the JSON received.",
+                ((JSONObject) responseBody).get("message"),
+                expectedMessage);
+    }
+
+    @Test
+    public void testCurrentGroupEmptyAPIKeySlash() throws IOException, ParseException{
+        Integer expectedCode = 401;
+        Integer expectedCod = 401;
+        String expectedMessage =
+                "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.";
+        String params = "/?appid=";
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertTrue("No message field is present",
+                ((JSONObject) responseBody).containsKey("message"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        assertEquals("Wrong message in the JSON received.",
+                ((JSONObject) responseBody).get("message"),
+                expectedMessage);
+    }
+
+    @Test
+    public void testCurrentGroupAPIKeyWithoutParamsSlash() throws IOException, ParseException{
+        Integer expectedCode = 200;
+        Integer expectedCod = 404;
+        String expectedMessage =
+                "Error: Not found city";
+        String params = "/?appid=" + Helpers.API_KEY;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertTrue("No message field is present",
+                ((JSONObject) responseBody).containsKey("message"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        assertEquals("Wrong message in the JSON received.",
+                ((JSONObject) responseBody).get("message"),
+                expectedMessage);
+    }
+
+    @Test
+    public void testCurrentGroupAPIKeyEmptyCityIDSlash() throws IOException, ParseException{
+        Integer expectedCode = 200;
+        Integer expectedCod = 404;
+        String expectedMessage =
+                "Error: Not found city";
+        String params = "/?id=&appid=" + Helpers.API_KEY;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertTrue("No message field is present",
+                ((JSONObject) responseBody).containsKey("message"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        assertEquals("Wrong message in the JSON received.",
+                ((JSONObject) responseBody).get("message"),
+                expectedMessage);
+    }
+
+    @Test
+    public void testCurrentGroupAPIKeyUnknownCityIDSlash() throws IOException, ParseException{
+        Integer expectedCode = 200;
+        Integer expectedCod = 404;
+        String expectedMessage =
+                "Error: Not found city";
+        String params = "/?id=JJJJJJJJJJJJ&appid=" + Helpers.API_KEY;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertTrue("No message field is present",
+                ((JSONObject) responseBody).containsKey("message"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        assertEquals("Wrong message in the JSON received.",
+                ((JSONObject) responseBody).get("message"),
+                expectedMessage);
+    }
+
+    @Test
+    public void testCurrentGroupNoAPIKeyCityIDSlash() throws IOException, ParseException{
+        Integer expectedCode = 401;
+        Integer expectedCod = 401;
+        String expectedMessage =
+                "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.";
+        String params = "/?id=2172797";
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertTrue("No message field is present",
+                ((JSONObject) responseBody).containsKey("message"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        assertEquals("Wrong message in the JSON received.",
+                ((JSONObject) responseBody).get("message"),
+                expectedMessage);
+    }
+
+    @Test
+    public void testCurrentGroupAPIKeyOneCityIDSlash() throws IOException, ParseException{
+        Integer expectedCode = 200;
+        Integer expectedCod = 200;
+        String params = "/?id=2172797&appid=" + Helpers.API_KEY;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        // TODO: write JSON validation
+    }
+
+    @Test
+    public void testCurrentGroupAPIKeySameCityIDsSlash() throws IOException, ParseException{
+        Integer expectedCode = 200;
+        Integer expectedCod = 200;
+        String params = "/?id=2172797,2172797&appid=" + Helpers.API_KEY;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        // TODO: write JSON validation
+    }
+
+    @Test
+    public void testCurrentGroupAPIKeyCityIDsSlash() throws IOException, ParseException{
+        Integer expectedCode = 200;
+        Integer expectedCod = 200;
+        String params = "/?id=2172797,2172796,2172795&appid=" + Helpers.API_KEY;
+        URL url = new URL(API_URL + params);
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Integer code = connection.getResponseCode();
+        Object responseBody = parser.parse(Helpers.getResponseBody(connection));
+        assertEquals("Wrong response code received",
+                code,
+                expectedCode);
+        assertTrue("No cod field is present",
+                ((JSONObject) responseBody).containsKey("cod"));
+        assertEquals("Wrong code in the JSON received.",
+                String.valueOf(((JSONObject) responseBody).get("cod")),
+                String.valueOf(expectedCod));
+        // TODO: write JSON validation
+    }
+
     public TestCurrentGroup() throws IOException{}
 }
